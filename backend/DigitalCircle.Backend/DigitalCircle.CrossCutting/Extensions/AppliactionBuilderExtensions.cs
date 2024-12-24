212194b8d9ed855250ec34ae22bfb081fe14caae
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:03:10 +0100

    feat: create IApplicationBuilder extensions methods for configurations

---
+++ backend/DigitalCircle.Backend/DigitalCircle.CrossCutting/Extensions/AppliactionBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace DigitalCircle.Backend.DigitalCircle.CrossCutting.Extensions;
+
+public static class AppliactionBuilderExtensions
+{
+    public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app,
+       IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        return app;
+    }
+
+    public static IApplicationBuilder UseAppCors(this IApplicationBuilder app)
+    {
+        app.UseCors(p =>
+        {
+            p.AllowAnyOrigin();
+            p.AllowAnyMethod();
+            p.AllowAnyHeader();
+        });
+        return app;
+    }
+
+    public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        return app;
+    }
+}
