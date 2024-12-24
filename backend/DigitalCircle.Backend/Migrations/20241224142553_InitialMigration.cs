1ca1b4ae1f5a77a46ac3e7dc48e3a7acd4309cff
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:27:17 +0100

    feat: create ef core migrations

---
+++ backend/DigitalCircle.Backend/Migrations/20241224142553_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalCircle.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB01",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    col_texto = table.Column<string>(type: "TEXT", nullable: false),
+                    col_dt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB01", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB01");
+        }
+    }
+}
