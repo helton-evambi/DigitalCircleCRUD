2fe0cab0956b801c4b92ae98a8b6238efec4e76b
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 14:55:10 +0100

    feat: create TB01 entity

---
+++ backend/DigitalCircle.Backend/DigitalCircle.Domain/Entities/TB01.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigitalCircle.Backend.DigitalCircle.Domain.Entities;
+
+public class TB01
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("col_texto")]
+    public string ColText { get; set; } = string.Empty;
+
+    [Column("col_dt")]
+    public DateTime ColDt { get; set; } = DateTime.Now;
+}
