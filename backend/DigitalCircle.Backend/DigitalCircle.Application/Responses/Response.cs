f55f59810838b24947496bf63f32811e745189c0
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:01:48 +0100

    feat: create response class

---
+++ backend/DigitalCircle.Backend/DigitalCircle.Application/Responses/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace DigitalCircle.Backend.DigitalCircle.Application.Responses;
+
+public class Response
+{
+    public string Status { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public Object Data { get; set; } = string.Empty;
+}
