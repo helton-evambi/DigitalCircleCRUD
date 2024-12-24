a14084208bf4c2542d1cf18bea2754fe904ee0dd
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:06:18 +0100

    feat: create repository interface

---
+++ backend/DigitalCircle.Backend/DigitalCircle.Domain/Interfaces/ITB01Repository.cs
@@ -0,0 +1,13 @@
+﻿using DigitalCircle.Backend.DigitalCircle.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace DigitalCircle.Backend.DigitalCircle.Domain.Interfaces;
+
+public interface ITB01Repository
+{
+    Task<IEnumerable<TB01>> GetAllAsync();
+    Task<TB01?> GetAsync(Expression<Func<TB01, bool>> predicate);
+    Task<TB01> CreateAsync(TB01 entity);
+    Task<TB01> UpdateAsync(TB01 entity);
+    Task<TB01?> DeleteAsync(TB01 entity);
+}
