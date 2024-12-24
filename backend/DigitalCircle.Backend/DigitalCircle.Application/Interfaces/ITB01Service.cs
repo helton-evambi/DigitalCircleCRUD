a60bcc1c32045ea82e444855b0a8e70d9a256323
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 14:58:14 +0100

    create TB01 service interface

---
+++ backend/DigitalCircle.Backend/DigitalCircle.Application/Interfaces/ITB01Service.cs
@@ -0,0 +1,12 @@
+﻿using DigitalCircle.Backend.DigitalCircle.Application.DTOs;
+using DigitalCircle.Backend.DigitalCircle.Application.Responses;
+namespace DigitalCircle.Backend.DigitalCircle.Application.Interfaces;
+
+public interface ITB01Service
+{
+    Task<IEnumerable<TB01ReadDto>> GetAll();
+    Task<Response> GetById(int id);
+    Task<Response> Add(TB01AddDto entityDto);
+    Task<Response> Update(TB01UpdateDto entityDto);
+    Task<Response> Remove(TB01DeleteDto entityDto);
+}
