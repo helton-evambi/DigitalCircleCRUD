a5e339036120fc6b4b6b630ecb94f45993056f0e
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:07:05 +0100

    feat: create application context

---
+++ backend/DigitalCircle.Backend/DigitalCircle.Infrastructure/Context/AppDbContext .cs
@@ -0,0 +1,15 @@
+﻿using DigitalCircle.Backend.DigitalCircle.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalCircle.Backend.DigitalCircle.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<TB01> TB01 { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
+
