73ec39df5b7f4dfde73086a4d9ec929ece124683
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:29:26 +0100

    refactor:  add extentensios methods in program class

--- backend/DigitalCircle.Backend/Program.cs
+++ backend/DigitalCircle.Backend/Program.cs
@@ -1,20 +1,27 @@
+using DigitalCircle.Backend.DigitalCircle.CrossCutting.Extensions;
+using DigitalCircle.Backend.DigitalCircle.CrossCutting.IoC;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDepencyInjection(builder.Configuration);
+builder.AddApiSwagger();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var enviroment = app.Environment;
+
+app.UseExceptionHandling(enviroment)
+    .UseSwaggerMiddleware()
+    .UseAppCors();
+
+app.UseHttpsRedirection();
 
 app.UseHttpsRedirection();
 
