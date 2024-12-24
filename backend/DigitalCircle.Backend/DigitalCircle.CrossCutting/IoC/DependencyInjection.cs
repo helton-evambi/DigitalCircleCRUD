d17de83d8845c39329f331256637b6c570f06ebc
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:05:44 +0100

    feat: extension to DI container service

---
+++ backend/DigitalCircle.Backend/DigitalCircle.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using DigitalCircle.Backend.DigitalCircle.Application.Interfaces;
+using DigitalCircle.Backend.DigitalCircle.Application.Services;
+using DigitalCircle.Backend.DigitalCircle.Domain.Interfaces;
+using DigitalCircle.Backend.DigitalCircle.Infrastructure.Context;
+using DigitalCircle.Backend.DigitalCircle.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalCircle.Backend.DigitalCircle.CrossCutting.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDepencyInjection(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite("Data Source=database.db"));
+
+
+        services.AddScoped<ITB01Repository, TB01Repository>();
+        services.AddScoped<ITB01Service, TB01Service>();
+
+        return services;
+    }
+}
