86dfade6bbd43c52d82573e98f1da8a7e4ef0c20
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-24 15:04:36 +0100

    feat: create WebApplicationBuilder extensions methods to configure swagger

---
+++ backend/DigitalCircle.Backend/DigitalCircle.CrossCutting/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace DigitalCircle.Backend.DigitalCircle.CrossCutting.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwagger();
+        return builder;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "DigitalCircleCRUD", Version = "v1" });
+     
+        });
+        return services;
+    }
+}
