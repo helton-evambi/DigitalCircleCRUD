e4d8d6e249bbcd8afe4ed6e1273287ce885a3cfe
Helton Evambi <helton.evambi@ocasogroup.co.ao>
2024-12-25 04:45:00 +0100

    feat: create text area component

---
+++ frontend/digitalcircle-frontend/src/app/components/text-area/text-area.component.ts
@@ -0,0 +1,47 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-text-area',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextAreaComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './text-area.component.html',
+  styleUrl: './text-area.component.scss',
+})
+export class TextAreaComponent implements ControlValueAccessor {
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() inputName: string = '';
+
+  value!: string;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+}
